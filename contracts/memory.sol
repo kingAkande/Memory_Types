7d67895f0ae959e9ebefd685c617f4e32a8abb97
Akande Sulaimon Olamilekan <122680161+kingAkande@users.noreply.github.com>
2024-03-06 21:31:59 +0100

    Update memory.sol

--- contracts/memory.sol
+++ contracts/memory.sol
@@ -1,34 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract memoryTpes {
 
-    event Withdrawal(uint amount, uint when);
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+        struct ND1students{
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
+             string name;
+             uint256 matric;
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+        ND1students public  record;
 
-        owner.transfer(address(this).balance);
-    }
+
+        function collation(string memory _name, uint256 _matric) external {
+
+                record.name =  _name;
+                record.matric = _matric;
+
+        }
+
+
+        function file()external view returns (ND1students memory){
+
+            return record;
+        }
+
 }
